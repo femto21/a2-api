4ffed1d116c785f11f84ad2efc3fc71769ebfb9d
Arpit Gahlot <arpitgahlot2003@gmail.com>
2026-06-30 23:15:10 -0300

    deploying

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import authRouter from "./routers/AuthRouters.js";
 import groupRouter from "./routers/GroupRouters.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(
   cors({
     origin: "http://localhost:5173", // allow the frontend origin
@@ -20,6 +22,6 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(3000, () => {
-  console.log("API on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`API on port ${PORT}`);
 });
